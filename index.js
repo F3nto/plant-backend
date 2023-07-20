bce6793ccdc78e73647d47f7f812575deadb290f
F3nto <pyaesonetun1141999@gmail.com>
2023-07-20 18:17:53 +0630

    push /

--- index.js
+++ index.js
@@ -28,10 +28,10 @@ app.use(cors(), (req, res, next) => {
 });
 
 
-app.use(`api/v1/categories`, CategoriesRoute);
-app.use(`api/v1/indoor-plants`, IndoorPlantRoute);
-app.use(`api/v1/fruit`, FruitPlantRoute);
-app.use(`api/v1/flower`, FlowerRoute);
+app.use(`/api/v1/categories`, CategoriesRoute);
+app.use(`/api/v1/indoor-plants`, IndoorPlantRoute);
+app.use(`/api/v1/fruit`, FruitPlantRoute);
+app.use(`/api/v1/flower`, FlowerRoute);
 
 
 app.listen(process.env.PORT, () => {
